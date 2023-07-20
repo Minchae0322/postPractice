631cc3e1e58be0baed8fa90787a755dd0b4f9bc5
Minchae0322 <jmcabc0322@gmail.com>
2023-07-20 20:14:59 +0900

    7.20

---
+++ front/src/views/AboutView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="about">
+    <h1>그냥 바뀌는 구나 실시간으로</h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
